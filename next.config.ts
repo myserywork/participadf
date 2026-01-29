cba9e712af4edd74733afb7c1400c2f7dc1b8a7c
pedro noronha <myserywork@gmail.com>
2026-01-29 20:45:39 -0300

    Test without PWA

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
-import withPWAInit from "@ducanh2912/next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  disable: process.env.NODE_ENV === "development",
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-});
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // Silencia o aviso do Turbopack
-  turbopack: {},
   images: {
     remotePatterns: [
       {
@@ -26,4 +12,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
